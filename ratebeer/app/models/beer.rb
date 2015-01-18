17becb69ffe00be91191fe21a593a6c66cdad9ff
Heidi Jauhiainen <wikgren@gmail.com>
2015-01-18 17:11:21 +0100

    Tehtava 4

--- ratebeer/app/models/beer.rb
+++ ratebeer/app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    count = 0.0
+    self.ratings.each do |rating|
+      sum = sum + rating.score
+      count += 1
+    end
+    return (sum/count).round(1)
+  end
 end
