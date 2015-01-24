a9ea253b18ecc8944f6e8af42f43d2336be67e3d
Heidi Jauhiainen <wikgren@gmail.com>
2015-01-24 15:52:40 +0200

    Tehtava 11

--- ratebeer/config/routes.rb
+++ ratebeer/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'breweries#index'
 
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to: 'ratings#new'
-  post 'ratings', to: 'ratings#create'
-
+#  get 'ratings', to: 'ratings#index'
+#  get 'ratings/new', to: 'ratings#new'
+#  post 'ratings', to: 'ratings#create'
+  resources :ratings, only: [:index, :new, :create, :destroy]
   resources :beers
   resources :breweries
 
