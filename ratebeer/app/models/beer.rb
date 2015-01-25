950c4dffe05d113af5be9d8f224ad90e1d1fda7d
Heidi Jauhiainen <wikgren@gmail.com>
2015-01-25 10:49:37 +0200

    Tehtavat 14-16

--- ratebeer/app/models/beer.rb
+++ ratebeer/app/models/beer.rb
@@ -1,10 +1,8 @@
 class Beer < ActiveRecord::Base
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-
-  def average_rating
-    return self.ratings.average(:score)
-  end
 
   def to_s
     return "#{self.name} - #{self.brewery.name}"
