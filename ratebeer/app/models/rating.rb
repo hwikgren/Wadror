3fbcd9483d645e5f6da6f03118039a9b444b92e6
Heidi Jauhiainen <wikgren@gmail.com>
2015-01-25 16:38:18 +0200

    Viikko 3 tehtava 3

--- ratebeer/app/models/rating.rb
+++ ratebeer/app/models/rating.rb
@@ -1,8 +1,9 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
+  belongs_to :user
 
   def to_s
-    beer = Beer.find self.beer_id
+    #beer = Beer.find self.beer_id
     "#{beer.name} #{self.score}"
   end
 end
--- ratebeer/lib/rating_average.rb
+++ ratebeer/lib/rating_average.rb
@@ -1,5 +1,5 @@
 module RatingAverage
   def average_rating
-    return ratings.average(:score)
+    return ratings.average(:score).round(2)
   end
 end
