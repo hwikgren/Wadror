9e192f97ed82afdddabf1d049891c25c4ef295ea
Heidi Jauhiainen <wikgren@gmail.com>
2015-01-30 15:13:52 +0200

    Viikko 3 tehtava 10

--- ratebeer/app/models/user.rb
+++ ratebeer/app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   include RatingAverage
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :password, length: { minimum: 4 }, format: { with: /\A(.*[A-Z].*)(.*\d.*)|(.*\d.*)(.*[A-Z].*)\z/, 
+    message: "has to contain at least one uppercase letter and one number" }
 
+  has_secure_password
   has_many :ratings
+  has_many :beers, through: :ratings
 end
