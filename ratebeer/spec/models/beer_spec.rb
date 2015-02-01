2d40d07da40bc8c9011122536f1d287b1e67323e
Heidi Jauhiainen <wikgren@gmail.com>
2015-02-01 18:48:57 +0200

    Viikko 4 tehtavat 1-2

---
+++ ratebeer/spec/models/beer_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Beer, :type => :model do
+
+  describe "with name and style" do
+    let(:beer){ Beer.create name:"Olut", style:"Lager" }
+
+    it "is saved" do
+      expect(beer).to be_valid
+      expect(Beer.count).to eq(1)
+    end
+  end
+
+  describe "with no name" do
+    let(:beer){Beer.create style:"Lager" }
+  
+    it "is not saved" do
+      expect(beer).not_to be_valid
+      expect(Beer.count).to eq(0)
+    end
+  end
+
+  describe "with no style" do
+    let(:beer){ Beer.create name:"Olut" }
+
+    it "is not saved" do
+      expect(beer).not_to be_valid
+      expect(Beer.count).to eq(0)
+    end
+  end
+end
