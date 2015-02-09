d446cad3092d7e009af29b5a42de98379c1a3332
Heidi Jauhiainen <wikgren@gmail.com>
2015-02-09 14:29:09 +0200

    viikko 5 tehtavat 2-3

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "Places" do
+  it "if one is returned by the API, is shown at the page" do
+    BeermappingApi.stub(:places_in).with("Kumpula").and_return(
+      [ Place.new(:name => "Oljenkorsi") ]
+    )
+    visit places_path
+    fill_in('city', with:'Kumpula')
+    click_button "Search"
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if several are returned by the API, are shown at the page" do
+    BeermappingApi.stub(:places_in).with("helsinki").and_return(
+     [ Place.new(:name => "Oljenkorsi"), Place.new(:name => "Kaisla") ]
+    )
+    visit places_path
+    fill_in('city', with:'helsinki')
+    click_button "Search"
+    expect(page).to have_content "Kaisla"
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if no are returned by the API, none are shown on the page" do
+    BeermappingApi.stub(:places_in).with("helsinki").and_return([])
+    visit places_path
+    fill_in('city', with:'helsinki')
+    click_button "Search"
+    expect(page).to have_content "No locations in helsinki"
+  end
+end
