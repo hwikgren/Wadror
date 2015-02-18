5fb1210aea590467ffc8516e1bff499d6105bd46
Heidi Jauhiainen <wikgren@gmail.com>
2015-02-18 17:53:08 +0200

    Viikko 6 korjaus

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    if user.iced
-      redirect_to :back, notice:"Your account is frozen, please contact the admin!"
-    elsif user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user, notice: "Welcome back!"
+    if user && user.authenticate(params[:password])
+      if user.iced
+        redirect_to :back, notice:"Your account is frozen, please contact the admin!"
+      else
+        session[:user_id] = user.id
+        redirect_to user, notice: "Welcome back!"
+      end
     else
       redirect_to :back, notice: "Username and/or password mismatch"
     end
